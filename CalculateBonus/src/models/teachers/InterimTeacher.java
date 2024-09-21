9b13e7f35f6923c3aea31a4fc003010fcbe5cd53
Anderson Lozano <adslozanov@gmail.com>
2024-09-21 16:46:32 -0500

    :tada: The branch for solving the exam challenges of the first module is created and unnecessary files are deleted.

--- CalculateBonus/src/models/teachers/InterimTeacher.java
+++
@@ -1,8 +0,0 @@
-package models.teachers;
-
-public class InterimTeacher extends Teacher {
-
-    public InterimTeacher(String name, String lastName, int age, String dni, String address, float salary) {
-        super(name, lastName, age, dni, address, salary);
-    }
-}
--- SingleOperations/src/MainApp.java
+++
@@ -1,8 +0,0 @@
-import domain.Operation;
-
-public class MainApp {
-    public static void main(String[] args) {
-        Operation app = new Operation();
-        app.setVisible(true);
-    }
-}
